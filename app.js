a00dbe871d9e84547330f889be8b90ac9916cca2
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-01-23 22:20:28 +0000

    started on the State

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+'use strict';
 const state = {
+  clickTracker: {
+    '0': null,
+    '1': null,
+    '2': null,
+    '3': null,
+    '4': null,
+    '5': null,
+    '6': null,
+    '7': null,
+    '8': null
+  },
+  isO: false    
 };
 
+const winningPos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
 // State modification functions
+function modifyState(){
+    
+}
+
 
 // Render functions
 
